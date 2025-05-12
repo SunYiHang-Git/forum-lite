947ff86874fb06df145b6f44859a5ed7c32bdf13
孙一航 <sunyihang@kingsware.cn>
2025-05-12 14:04:59 +0800

    修复昵称

--- src/views/Article/components/CommentArticle.vue
+++ src/views/Article/components/CommentArticle.vue
@@ -53,9 +53,7 @@ function handleUserNameReply(item: any) {
   const isUserRepeated = userNameIdRepeatList.value.includes(item.userId)
   const isReplyRepeated = userNameIdRepeatList.value.includes(item.replyPersonUserId)
   // 处理 userName
-  const userResult = isUserRepeated
-    ? processName(item.userName)
-    : { name: getSplitStrName(item.getSplitStrName), suffix: '' }
+  const userResult = isUserRepeated ? processName(item.userName) : { name: getSplitStrName(item.userName), suffix: '' }
   item.userName = userResult.name
   item.suffix = userResult.suffix
   // 处理 replyPerson
