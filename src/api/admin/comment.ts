2724f323855f592e1c9b7bd84e6765e41454ecaa
孙一航 <sunyihang@kingsware.cn>
2025-06-06 10:55:47 +0800

    评论管理

---
+++ src/api/admin/comment.ts
@@ -0,0 +1,31 @@
+import { callServerFunc, SQLTable } from '@ksware/micro-lib-web-temp'
+
+/** 获取评论列表 */
+export const RPAReplyQueryLiteAPI = async (params: any) => {
+  const { data }: any = await callServerFunc('TRPADM', 'RPAReplyQueryLite', params, { isShowLoading: true })
+  const table = new SQLTable(data.k_forum_reply)
+  const rows: any[] = []
+  while (!table.eof()) {
+    const id = table.s('ID')
+    const row = {
+      id,
+      title: table.s('Title'),
+      user: table.s('UserName'),
+      time: table.s('Time'),
+      content: table.s('Content'),
+      tag: table.s('tag'),
+    }
+    rows.push(row)
+    table.next()
+  }
+  return { rows, total: data.DataCount }
+}
+
+/** 修改帖子信息 */
+// export const RPAEditPostsLiteAPI = async (params: any) => {
+//   await callServerFunc('TRPADM', 'RPAEditPostsLite', params)
+// }
+/** 删除评论 */
+export const RPADelReplyLiteAPI = async (params: any) => {
+  await callServerFunc('TRPADM', 'RPADelReplyLite', params)
+}
--- src/views/Admin/Comment/index.vue
+++ src/views/Admin/Comment/index.vue
@@ -1,12 +1,137 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { RPADelReplyLiteAPI, RPAReplyQueryLiteAPI } from '@/api/admin/comment'
+import { KMessageBox } from '@ksware/ksw-ux'
+import { ref } from 'vue'
+
+const widgets: string[] = ['search', 'refresh', 'transfer', 'custom1', 'sizeControl']
+
+/** 搜索类型 */
+const selectType = [
+  { label: '标题搜索', value: 'Title' },
+  { label: '内容搜索', value: 'Content' },
+  { label: '用户搜索', value: 'UserName' },
+]
+/** 选中搜索类型值 */
+const selectTypeValue = ref('Title')
+
+/** 搜索值 */
+const searchValue = ref('')
+
+const currentPage = ref(1)
+const pageSize = ref(20)
+const pageTotal = ref(0)
+
+const tableData = ref<any[]>([])
+const column = ref([
+  {
+    field: 'user',
+    title: '用户姓名',
+    width: '150',
+  },
+  {
+    field: 'title',
+    title: '帖子标题',
+  },
+  {
+    field: 'content',
+    title: '评论内容',
+  },
+  {
+    field: 'time',
+    title: '评论时间',
+    align: 'center',
+    width: '200',
+  },
+  {
+    field: 'opt',
+    title: '操作',
+    width: '100',
+    align: 'center',
+    fixed: 'right',
+  },
+])
+
+const reset = () => {
+  selectTypeValue.value = 'Title'
+  searchValue.value = ''
+  currentPage.value = 1
+  searchTable()
+}
+const searchTable = async () => {
+  const params = {
+    Keyword: searchValue.value,
+    KeywordType: selectTypeValue.value,
+    PageNum: currentPage.value - 1,
+    PageSize: pageSize.value,
+  }
+  const { rows, total } = await RPAReplyQueryLiteAPI(params)
+  tableData.value = rows
+  pageTotal.value = total
+}
+
+function initWindow() {
+  currentPage.value = 1
+  searchTable()
+}
+initWindow()
+
+const onDelComment = async (row: any) => {
+  await KMessageBox.confirm('确认要删除该评论吗?', '删除', {
+    confirmButtonText: '确定',
+    cancelButtonText: '取消',
+    type: 'warning',
+  })
+  const params = { ReplyID: row.id }
+  await RPADelReplyLiteAPI(params)
+  searchTable()
+}
+</script>
 
 <template>
-  <div class="comment-page">评论管理</div>
+  <div class="comment-page">
+    <div class="header-search">
+      <k-select v-model="selectTypeValue" clearable placeholder="是否申请" style="width: 180px">
+        <k-option v-for="item in selectType" :key="item.value" :label="item.label" :value="item.value" />
+      </k-select>
+      <KInput v-model="searchValue" style="width: 180px" placeholder="搜索内容" @keydown.enter="searchTable" />
+      <k-button icon-left="IconSearch" main @click="searchTable">搜索</k-button>
+      <k-button icon-left="IconRotateClockwise" main @click="reset">重置</k-button>
+    </div>
+    <div class="main-box">
+      <div class="table-box">
+        <k-tree-table ref="tableTreeRef" :widgets="widgets" :data="tableData" :column="column" @refresh="initWindow">
+          <template #opt="{ row }">
+            <k-button main type="danger" @click="onDelComment(row)">删除</k-button>
+          </template>
+        </k-tree-table>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 .comment-page {
   width: 100%;
   height: 100%;
+  .header-search {
+    display: flex;
+    align-items: center;
+    justify-content: start;
+    gap: 10px;
+    width: 100%;
+    height: 60px;
+  }
+  .main-box {
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    align-items: center;
+    width: 100%;
+    height: calc(100vh - 130px);
+    .table-box {
+      height: 100%;
+      width: 100%;
+    }
+  }
 }
 </style>
