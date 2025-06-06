40e96e8683bf50f7cf5592adb5301a27188f1d2d
孙一航 <sunyihang@kingsware.cn>
2025-06-06 11:54:31 +0800

    轮播管理-暂无接口

--- src/views/Admin/Banner/index.vue
+++ src/views/Admin/Banner/index.vue
@@ -1,8 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const column = ref([])
+const tableData = ref<any[]>([])
+function initWindow() {}
+</script>
 
 <template>
   <div class="banner-page">
-    <div class="header-search">轮播管理</div>
+    <div class="header-search">
+      <k-button icon-left="IconRotateClockwise" main size="sm" @click="initWindow">刷新</k-button>
+      <k-button icon-left="IconAdd" main size="sm" @click="initWindow">新增</k-button>
+    </div>
+    <div class="main-box">
+      <div class="table-box"></div>
+    </div>
   </div>
 </template>
 
@@ -19,10 +31,6 @@
     height: 60px;
   }
   .main-box {
-    display: flex;
-    flex-direction: column;
-    justify-content: start;
-    align-items: center;
     width: 100%;
     height: calc(100vh - 130px);
     .table-box {
